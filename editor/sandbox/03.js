e622e4bbc117dbe16b1840a8dee8ea19a3e02036
ch3coohlink <chiiicoohlink@gmail.com>
2024-06-08 22:21:12 +0800

    basic worker dispatch & monitoring

--- editor/sandbox/03.js
+++ editor/sandbox/03.js
@@ -1,8 +1,50 @@
-// 03 - basic worker dispatch
+// 03 - basic worker dispatch & monitoring
+const debounce = (f, t = 100, i) => (...a) =>
+  (clearTimeout(i), i = setTimeout(() => f(...a), t))
+const watches = new Map
+// TODO: watch file change
+const readfile = log
 
+const startfile = 'index.js'
+const content = `
+const f = ()=>{for(let i=0;i<10000000000;i++){}}
+const n = ()=>performance.now()/1000
+setInterval(() => {
+  log('start work')
+  let s = n()
+  f()
+  log('finish work', n() - s)
+}, 2000)
+f()
+`
 
+let alive = performance.now()
+const light = document.createElement('div')
+light.textContent = '🟢'
+document.body.append(light)
 
 const w = new Worker('editor/sandbox/worker.js')
-w.addEventListener('message', e => {
-
+const send = w.postMessage.bind(w)
+let watch = new Set([startfile])
+const reload = debounce(() => {
+  watch = new Set([startfile])
+  send({ command: 'reload' })
 })
+watches.set(w, { watch, reload })
+w.addEventListener('message', e => {
+  const o = e.data
+  if (o.command === 'init' || o.command === 'load') {
+    try {
+      const p = path.resolve(__dirname, o.path)
+      const f = readfile(p); watch.add(o.path)
+      send({ command: o.command, path: o.path, content: f })
+    } catch (e) {
+      send({ command: o.command + 'fail', error: e.message, path: o.path })
+    }
+  } else if (o.command === 'heartbeat') {
+    alive = performance.now()
+    light.textContent = '🟢'
+  }
+})
+setInterval(() => alive + 1100 < performance.now() ? light.textContent = '🔴' : 0, 1000)
+send({ command: 'init', path: startfile, content })
--- editor/sandbox/worker.js
+++ editor/sandbox/worker.js
@@ -1,7 +1,7 @@
 let $ = {}; {
-  window.log = console.log
-  window.dlog = console.dir
-  window.__require = base => async path => {
+  globalThis.log = console.log
+  globalThis.dlog = console.dir
+  globalThis.__require = base => async path => {
     let b = base.split('/'); path = path.split('/'); for (const p of path) {
       if (p === '..') { b.pop() } else if (p !== '.') { b.push(p) }
     } path = b.filter(v => v).join('/'); if (loaded.has(path)) { return }
@@ -14,33 +14,24 @@ let $ = {}; {
       `const __dirname = '${path.split('/').slice(0, -1).join('/')}'\n` +
       `const require = __require(__dirname)\n` + `with($) {\n${src}\n}`)($)))
 
-  let waitload = new Map, inited = false
+  let waitload = new Map
   let load = async path => new Promise((res, rej) => {
-    ws.send(JSON.stringify({ command: 'load', path }))
+    send({ command: 'load', path })
     waitload.set(path, { res, rej })
   })
 
-  const sp = new URLSearchParams(location.search)
-  const initpath = sp.get('load') ?? sp.get('init')
-  document.title = decodeURI(initpath)
-  let ws, connect = () => {
-    ws = new WebSocket((window.location.protocol === 'https:'
-      ? 'wss:' : 'ws:') + '//' + window.location.host)
-    ws.onopen = () => ws.send(JSON.stringify({ command: 'init', path: initpath }))
-    ws.onmessage = e => {
-      const o = JSON.parse(e.data)
-      if (o.command === 'init' && !inited) {
-        inited = true, exec(initpath, o.content)
-      } else if (o.command === 'load') {
-        waitload.get(o.path).res(o.content)
-        waitload.delete(o.path)
-      } else if (o.command === 'loadfail') {
-        waitload.get(o.path).rej(o.error)
-        waitload.delete(o.path)
-      } else if (o.command === 'reload') { location.reload() }
-      else if (o.command === 'initfail') { console.error(o.error) }
+  const send = postMessage
+  addEventListener('message', e => {
+    const o = e.data
+    if (o.command === 'init') { exec(o.path, o.content) }
+    else if (o.command === 'initfail') { console.error(o.error) }
+    else if (o.command === 'load') {
+      waitload.get(o.path).res(o.content)
+      waitload.delete(o.path)
+    } else if (o.command === 'loadfail') {
+      waitload.get(o.path).rej(o.error)
+      waitload.delete(o.path)
     }
-    ws.onclose = connect
-  }; connect(), setInterval(() => ws ? ws.send(
-    JSON.stringify({ command: 'heartbeat' })) : 0, 1000 * 60)
+  })
+  setInterval(() => send({ command: 'heartbeat' }), 500)
 }
