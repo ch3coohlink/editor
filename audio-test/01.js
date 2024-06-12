b251b317402ae8552b1cac020984da01e9a64dd6
ch3coohlink <chiiicoohlink@gmail.com>
2024-06-12 16:46:45 +0800

    ringbuffer

--- audio-test/01.js
+++ audio-test/01.js
@@ -1,55 +1,25 @@
 // 01 - basic buffer setup
 
 {
-  const global = globalThis
-  let nextHandle = 1; // Spec says greater than zero
-  const tasksByHandle = {};
-  let currentlyRunningATask = false;
-  const runIfPresent = handle => {
-    // From the spec: "Wait until any invocations of this algorithm started before this one have completed."
-    // So if we're currently running a task, we'll need to delay this invocation.
-    if (currentlyRunningATask) {
-      setTimeout(runIfPresent, 0, handle)
-    } else {
-      let task = tasksByHandle[handle]
-      if (task) {
-        currentlyRunningATask = true
-        try {
-          task.callback(...task.args)
-        } finally {
-          clearImmediate(handle)
-          currentlyRunningATask = false
-        }
-      }
-    }
-  }
-
-  const messagePrefix = "setImmediate$" + Math.random() + "$"
-  // {capture, once, passive, signal}
-  addEventListener("message", event => {
-    if (event.source === global &&
-      typeof event.data === "string" &&
-      event.data.indexOf(messagePrefix) === 0) {
-      runIfPresent(+event.data.slice(messagePrefix.length));
-    }
-  }, false)
-  const registerImmediate = h =>
-    global.postMessage(messagePrefix + h, "*")
-
-  $.setImmediate = (cb, ...a) => {
-    // Callback can either be a function or a string
-    if (typeof cb !== "function") { cb = new Function("" + cb) }
-    const task = { callback: cb, args: a }
-    tasksByHandle[nextHandle] = task
-    registerImmediate(nextHandle)
-    return nextHandle++
-  }
-  $.clearImmediate = h => delete tasksByHandle[h]
+  let i = 1, crt = false // currently running a task
+  const tasks = {}, run = h => {
+    if (crt) { return setTimeout(run, 0, h) }
+    let t = tasks[h]; if (!t) { return }
+    crt = true; try { t.f(...t.a) }
+    finally { delete tasks[h], crt = false }
+  }, p = "setImmediate$" + Math.random() + "$"
+  addEventListener("message", e => {
+    if (e.source === globalThis && typeof e.data === "string"
+      && e.data.indexOf(p) === 0) { run(+e.data.slice(p.length)) }
+  }); const rg = h => postMessage(p + h, "*")
+  $.setImmediate = (f, ...a) => (tasks[i] = { f, a }, rg(i), i++)
+  $.clearImmediate = h => delete tasks[h]
 }
 
 const wa = new AudioContext
-const ringbuffersize = 512
-const timelimit = ringbuffersize / wa.sampleRate * 1000
+const ringbuffersize = 1024 * 2
+const timelimit = ringbuffersize / wa.sampleRate
+log('buffer time', timelimit)
 const bf = wa.createBuffer(1, ringbuffersize * 2, wa.sampleRate)
 const sn = wa.createBufferSource()
 sn.buffer = bf
@@ -57,36 +27,30 @@ sn.loop = true
 sn.connect(wa.destination)
 sn.start(0)
 
-const now = performance.now.bind(performance)
+const now = () => performance.now() / 1000
 
-let t = 0
-let i = 0
+let t = now(), i = false
 const upd = () => {
   let b = bf.getChannelData(0)
-  let off = i * ringbuffersize
-  log('update')
+  let off = (i ? 1 : 0) * ringbuffersize; i = !i
+  const l = 0.1, pt = t, dt = 1 / wa.sampleRate
+  for (let i = 0, t = pt; i < ringbuffersize; i++, t += dt) {
+    b[off + i] = (t % 0.01 > 0.005) ? l : -l
+  }
 }
 
-
-
-let r = false
-let pt = now()
-// const tm = setImmediate
-const tm = setTimeout
+// const scb = setImmediate
+const scb = setTimeout
 const f = () => {
-  const ct = now()
-  log((ct - pt).toFixed(1))
-  pt = ct
-  // log(t - ct, t)
-  // if (ct > t) {
-  //   upd(), t += timelimit
-  // }
-  // else if (t - ct < 1) { r = false }
-  tm(f)
+  if (t - now() < timelimit * .5) {
+    upd(), t += timelimit
+  }
+  scb(f)
 }
-tm(f)
+scb(f)
 
 addEventListener('pointerdown', () => {
   log('start')
   wa.resume()
+  t = now()
 })
